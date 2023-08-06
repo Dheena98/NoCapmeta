074070fffc4364ad0f00f3bd79a25a06ce31ab91
Dheena <50746101+Dheena98@users.noreply.github.com>
2023-08-06 23:18:55 +0530

    Update AvatarCustomization.js

--- src/components/AvatarCustomization.js
+++ src/components/AvatarCustomization.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
 const AvatarCustomization = () => {
-  // State to store avatar customization data
   const [avatarData, setAvatarData] = useState(null);
 
   useEffect(() => {
